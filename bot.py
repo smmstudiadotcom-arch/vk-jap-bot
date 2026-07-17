b05de71cdcb729e94af71a12ab1f8ee78b4dd5f7
smmstudiadotcom-arch <smmstudiadotcom@gmail.com>
2026-07-17 12:37:18 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,6 +42,7 @@ VK_PERMANENT = [
 VK_TEMPORARY = [
     # (страница, дата_окончания, мин, макс)
     ("secretsofthewallet",  "2026-08-20", 40, 69),
+    ("sportrekordrussia",   "2026-08-17", 40, 69),
     ("vica.nikiforova",     "2026-09-20", 50, 70),
 ]
 
@@ -932,6 +933,7 @@ SP_PAGES          = [
     ("smm.studia",         7, 14, None),
     ("pro_samorasvitie",   7, 14, None),
     ("secretsofthewallet", 7, 11, "2026-08-20"),
+    ("sportrekordrussia",  7, 11, "2026-08-17"),
     ("vica.nikiforova",    7, 15, "2026-09-20"),
 ]
 SP_PHOTO_ALBUMS   = [
